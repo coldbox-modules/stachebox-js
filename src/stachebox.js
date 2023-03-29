e1957cffb6506a9f375bf7d76286387041f997a0
jclausen <jclausen@ortussolutions.com>
2023-03-29 14:22:53 -0400

    Converts log format to ECS

--- src/stachebox.js
+++ src/stachebox.js
@@ -8,87 +8,152 @@ class Stachebox {
 			endpoint : "/stachebox/api/v1/logs",
 			token : "",
 			application : "stachebox",
-			release : "",
+			release : null,
 			type : "javascript",
-			environment : "production"
+			environment : "production",
+			transmissionMethod : "api"
 		}
-	
+
 		Object.assign( this._config, options );
-	
+
 	}
 
 	log( entry ){
 		var logEntry;
-		if( typeof entry === 'string' ){
-			entry = {
-				"message" : entry
-			};
-		}
+		var creationTime = new Date().toISOString().split('.')[0]+"Z";
 		if( entry instanceof Error ){
 			let stackFrames = ErrorStackParser.parse( entry );
 			logEntry = {
-				application : this._config.application,
-				level : entry.level || 'ERROR',
-				type : this._config.type,
-				release : this._config.release,
-				message : entry.message,
-				stacktrace : entry.stack,
-				frames : stackFrames.map(
-					frame => {
-						return {
-							columnNumber : frame.columnNumber,
-							fileName : frame.fileName,
-							functionName : frame.functionName,
-							lineNumber : frame.lineNumber,
-							source : frame.source
-						};
-					}
-				),
-				event : {
-					name :  stackFrames[ 0 ].functionName,
-					route : window ? window.location.pathname : "",
-					routed_url : window ? window.location.href : "",
-					environment : this._config.environment
+				"@timestamp" : creationTime,
+				"labels" : {
+					"application" : this._config.application,
+					"environment" : this._config.environment.toLowerCase()
+				},
+				"log" : {
+					"level" : entry.level || 'ERROR',
+					"logger": "stachebox-npm",
+				},
+				"package" : {
+					"type" : this._config.type,
+					"version" : this._config.release,
+					"path" : window ? window.location.pathname : null
+				},
+				"event" : {
+					"created" : creationTime
+				},
+				"url" : {
+					"domain" : window ? window.location.hostname : null,
+					"path" : window ? window.location.pathname : null,
+					"port" : window ? window.location.port : null,
+					"query" : window ? window.location.search : null,
+					"scheme" : window ? window.location.protocol : null
+				},
+				"message" : entry.message,
+				"host" : {
+					"name" : window ? window.location.hostname : null
+				},
+				"user_agent" : {
+					"original" : window ? window.navigator.userAgent : null
+				},
+				"error" : {
+					"level"   : entry.level || 'ERROR',
+					"type" 	  : entry.constructor ? entry.constructor.name : 'Error',
+					"message" : entry.message,
+					"stack_trace" : entry.stack,
+					"frames" : stackFrames.map(
+						frame => {
+							return {
+								"abs_path"     : frame.fileName,
+								"filename"     : frame.fileName,
+								"functionname" : frame.functionName,
+								"lineno"       : frame.lineNumber,
+								"columnno" : frame.columnNumber,
+								"pre_context"  : [],
+								"context_line" : frame.source,
+								"post_context" : [],
+							};
+						}
+					)
 				}
 			}
 		} else {
-			logEntry = entry;
+				logEntry = {
+					"@timestamp" : creationTime,
+					"labels" : {
+						"application" : this._config.application,
+						"environment" : this._config.environment.toLowerCase()
+					},
+					"log" : {
+						"level" : "ERROR",
+					},
+					"message" : typeof entry === 'string' ? entry : entry.message,
+					"package" : {
+						"type" : this._config.type,
+						"version" : this._config.release,
+						"path" : window ? window.location.pathname : null
+					},
+					"event" : {
+						"created" : creationTime
+					},
+					"url" : {
+						"domain" : window ? window.location.hostname : null,
+						"path" : window ? window.location.pathname : null,
+						"port" : window ? window.location.port : null,
+						"query" : window ? window.location.search : null,
+						"scheme" : window ? window.location.protocol : null
+					},
+					"host" : {
+						"name" : window ? window.location.hostname : null
+					},
+					"user_agent" : {
+						"original" : window ? window.navigator.userAgent : null
+					}
+				};
+				if( typeof entry === 'object' ){
+					Object.assign( logEntry, entry );
+				}
 		}
-	
+
 		return axios.post(
 			this._config.endpoint,
-			JSON.stringify( { entry : logEntry } ),
+			this._config.transmissionMethod == "api" ? JSON.stringify( { entry : logEntry } ) : JSON.stringify( logEntry ),
 			{ headers : { "Authorization" : "Bearer " + this._config.token, "Content-Type" : 'application/json' } }
 		);
 	}
 
-	error( entry ){ 
+	error( entry ){
 		if( typeof entry === 'string' ){
 			entry = {
 				"message" : entry
 			};
 		}
-		entry.level = 'ERROR';
+		entry.log = {
+			level : 'ERROR'
+		};
 		return this.log( entry );
 	}
 
-	warn( entry ){ 
+	warn( entry ){
 		if( typeof entry === 'string' ){
 			entry = {
 				"message" : entry
 			};
 		}
-		entry.level = 'WARN';
+		entry.log = {
+			level : 'WARN'
+		};
 		return this.log( entry );
 	}
 
-	info( entry ){ 
+	info( entry ){
 		if( typeof entry === 'string' ){
 			entry = {
 				"message" : entry
 			};
 		}
-		entry.level = 'INFO';
+		entry.log = {
+			level : 'INFO'
+		};
 		return this.log( entry );
 	}
 }
